24f1c0588170edc5f8da0d82ea12899333d1d129
Sandara <Tababan25@gmail.com>
2024-09-18 03:36:18 -0700

    6_Updated_MovieList

---
+++ 6_MovieList.py
@@ -0,0 +1,25 @@
+movie_list = ["Avengers", "Innocence", "Ballerina", "Mouse", "Uglies", 
+              "Rookie", "Psychokinesis", "Bros", "Ladybrinth", "Pirates", 
+              "Pandora", "Decibel", "Always", "1987", "Silenced", "Dream", "Collectors", 
+              "Unlocked", "Believer", "Hard Hit"]
+
+# 2
+print(movie_list)
+
+# 3
+print(movie_list[12])
+
+# 4
+movie_list[15] = "Inception"
+
+# 5
+del movie_list[18]
+
+# 2 
+print(movie_list)
+
+# 6
+print(movie_list[8:13])
+
+# 7
+print("the last index is ", movie_list[-1])
