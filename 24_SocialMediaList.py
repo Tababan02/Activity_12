45d5b1b40e54b3917330da7839f8d744805b5f71
Sandara <Tababan25@gmail.com>
2024-09-18 04:07:21 -0700

    24_SocMedList

---
+++ 24_SocialMediaList.py
@@ -0,0 +1,20 @@
+social_media_list = ["Facebook", "Threads", "Twitter", "Linkedln", "WeChat", "Discord", "Telegram", "Tumblr", "WhatsApp", "YouTube"]
+
+
+# 2
+print(social_media_list)
+
+# 3
+print(social_media_list[5])
+
+# 4
+social_media_list[3] = "Instagram"
+
+# 5
+del social_media_list[8]
+
+# 2 
+print(social_media_list)
+
+# 6
+print("The last index is ", social_media_list[-1])
