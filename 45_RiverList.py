928a37360a3e98b78c2323c9639f907ca5963094
Sandara <Tababan25@gmail.com>
2024-09-18 04:20:44 -0700

    45_Updated_RiverList

---
+++ 45_RiverList.py
@@ -0,0 +1,21 @@
+river_list = ["Amazon River", "Yangtze River", "Mississipi River", "Yenisei River", "Yellow River", "Ob-Irtysh River", "Lena River", "Mekong River", "Ganges River", "Danube River"]
+
+
+
+# 2
+print(river_list)
+
+# 3
+print(river_list[6])
+
+# 4
+river_list[8] = "Nile River"
+
+# 5
+del river_list[9]
+
+# 2
+print(river_list)
+
+# 6
+print("The last index is " ,river_list[-1])
