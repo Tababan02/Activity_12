4fb47cb413672ef8419896d14dfd698608e8b1ab
Sandara <Tababan25@gmail.com>
2024-09-18 04:22:54 -0700

    48_Updated_BridgeList

---
+++ 48_BridgeList.py
@@ -0,0 +1,24 @@
+famous_bridge_list = ["Brooklyn Bridge", "Tower Bridge", "Sydney Harbour Bridge", "Pont du Gard", "Charles Bridge", "Milennium Bridge", "Akashi Kaikyō Bridge", "Verrazano-Narrows", "Rio Niterói Bridge", 
+"Pont Neuf"]
+
+
+
+
+
+# 2
+print(famous_bridge_list)
+
+# 3
+print(famous_bridge_list[3])
+
+# 4
+famous_bridge_list[6] = "Golden Gate Bridge" 
+
+# 5
+del famous_bridge_list[7]
+
+# 2 
+print(famous_bridge_list)
+
+# 6
+print("The last index is ", famous_bridge_list[-1])
