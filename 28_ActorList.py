4366bf68906a5d2fda60734389c6b02839ea34cf
Sandara <Tababan25@gmail.com>
2024-09-18 04:10:09 -0700

    28_Updated_ActorList

---
+++ 28_ActorList.py
@@ -0,0 +1,19 @@
+actor_list = ["Tom Hanks", "Denzel Washington", "Meryl Streep", "Johnny Depp", "Brad Pitt", "Will Smith", "Robert De Niro", "Nicole Kidman", "Harrison Ford", "Ryan Reynolds"]
+
+# 2 
+print(actor_list)
+
+# 3
+print(actor_list[7])
+
+# 4
+actor_list[3] = "Leonardo DiCarpio"
+
+# 5
+del actor_list[8]
+
+# 2
+print(actor_list)
+
+# 6
+print("The last index is ", actor_list[-1]) 
