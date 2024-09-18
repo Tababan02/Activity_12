c0b63b4ab105fe67f644218125036843b183e10c
Sandara <Tababan25@gmail.com>
2024-09-18 03:48:21 -0700

    14_Updated_CompanyList

---
+++ 14_CompanyList.py
@@ -0,0 +1,19 @@
+company_list = ["Nike Inc.,", "Nortek Inc", "NSTAR", "Nvidia Corp", "Office Depot Inc.", "Olin Corp.", "Omnicare Inc", "Pfizer Inc", "Phar Mor Inc.", "Perini Corp"] 
+
+# 2
+print(company_list)
+
+# 3
+print(company_list[7])
+
+# 4
+company_list[5] = "Apple"
+
+# 5
+del company_list[8]
+
+# 2 
+print(company_list)
+
+# 6
+print("the last index is ", company_list[-1])
