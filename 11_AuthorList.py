b916bac12cdb91e7050910fa05aad0aa25d38476
Sandara <Tababan25@gmail.com>
2024-09-18 03:45:34 -0700

    11_Updated_AuthorList

---
+++ 11_AuthorList.py
@@ -0,0 +1,23 @@
+author_list= ["George Orwell", "Stephen King", "Albert Camus", "Virginia Woolf", "Leo Tolstoy", "Herman Melville", "Agatha CHristine", "John Steinback", "John Bunyan", "C.S. Lewis", "Alexandre Dumas", "Jane Austen"] 
+
+
+# 2
+print(author_list)
+
+# 3
+print(author_list[6])
+
+# 4
+author_list[4] = "Mark Twain"
+
+# 5
+del author_list[10]
+
+# 2 
+print(author_list)
+
+# 6
+print(author_list[3:8])
+
+# 7
+print("the last index is ", author_list[-1])
