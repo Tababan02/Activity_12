9b96ef0415ce16e8f078482bf1e9e4c9e028a1bc
Sandara <Tababan25@gmail.com>
2024-09-18 03:23:17 -0700

    2_Updated_AnimalList

---
+++ 2_AnimalList.py
@@ -0,0 +1,21 @@
+animal_list = ["dog", "cat", "giraffe", "goat", "tiger", "panda", "bears", "lion", "horse", "wolf", "dinosaur", "hamster", "hedgehog"]
+
+# 2
+print(animal_list)
+
+# 3
+print(animal_list[3])
+
+# 4
+animal_list[6] = "Elephant" 
+
+# 5
+del animal_list[8]
+
+# 2
+print(animal_list)
+
+
+# 6
+print("last index is", animal_list[-1])
+
