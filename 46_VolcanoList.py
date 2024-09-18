48315b0bab31f31b6c72aa1f6c118ff38ace07a3
Sandara <Tababan25@gmail.com>
2024-09-18 04:21:15 -0700

    46_Updated_VolcanoList

---
+++ 46_VolcanoList.py
@@ -0,0 +1,19 @@
+volcano_list = ["Mount Fuji", "Kilauea", "mount st helens", "mount killmanjaro", "mount etna", "mount rainer", "mount pinatubo", "villarica", "mount aso", "tungurahua"]
+
+# 2
+print(volcano_list)
+
+# 3
+print(volcano_list[4])
+
+# 4
+volcano_list[3] = "Mount Vesuvius"
+
+# 5
+del volcano_list[5]
+
+# 2
+print(volcano_list)
+
+# 6
+print("The last index is ", volcano_list[-1])
