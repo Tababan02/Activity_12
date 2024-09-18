6cda1dc856f3795caf3d79f7738a557dd5e8e83b
Sandara <Tababan25@gmail.com>
2024-09-18 03:47:30 -0700

    13_Updated_MountainList

---
+++ 13_MountainList.py
@@ -0,0 +1,19 @@
+mountain_list = ["Nanga Parbat", "K2", "Denali", "Aconcagua", "Kamet", "Elbrus", "Mount Apo", "Mayon Volcano"]
+
+# 2
+print(mountain_list)
+
+# 3
+print(mountain_list[5])
+
+# 4 
+mountain_list[3] = "Everest"
+
+# 5
+del mountain_list[6]
+
+# 2
+print(mountain_list)
+
+# 6
+print("The index is ", mountain_list[-1])
