7cab7225aedaf2722eb293494499f15fff3ab6a8
Sandara <Tababan25@gmail.com>
2024-09-18 04:12:08 -0700

    30_Updated_ToolList

---
+++ 30_ToolList.py
@@ -0,0 +1,19 @@
+tool_list = ["Screwdriver", "Tape Measure", "Saw", " Chisel", "Level", "Drill", "Jigsaw", "Pliers", "Sander", "Router"] 
+
+# 2
+print(tool_list)
+
+# 3
+print(tool_list[4])
+
+# 4
+tool_list[7] = "Hammer" 
+
+# 5
+del tool_list[5]
+
+# 2 
+print(tool_list)
+
+# 6
+print("The last index is ", tool_list[-1])
