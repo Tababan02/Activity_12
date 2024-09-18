2d078f5a3ea1c72d18e746f548089ffe21469c55
Sandara <Tababan25@gmail.com>
2024-09-18 04:09:35 -0700

    27_Updated_BuildingList

---
+++ 27_BuildingList.py
@@ -0,0 +1,19 @@
+building_list = ["Pyramid of Giza", "Seoul Tower", "Big Ben", "Machu Picchu", "Empire State Building", "Petra", "Fallingwater", "Chichen Itza"]
+
+# 2
+print(building_list)
+
+# 3
+print(building_list[5])
+
+# 4
+building_list[2] = "Eiffel Tower"
+
+# 5
+del building_list[6]
+
+# print
+print(building_list)
+
+# 6
+print("the last index is ", building_list[-1])
