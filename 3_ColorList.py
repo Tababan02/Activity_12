a20eaa8a6d3ba6571f0f01d264736541a22952aa
Sandara <Tababan25@gmail.com>
2024-09-18 03:31:05 -0700

    3_Updated_ColorList

---
+++ 3_ColorList.py
@@ -0,0 +1,23 @@
+color_list = ["black", "white", "gray", "pink", "red", "blue", "green", "cyan", "orange", "brown", "yellow", "gold", "offwhite", "indigo", "violet"]
+
+
+# 1
+print(color_list)
+
+# 2 
+print(color_list[10])
+
+# 3
+color_list[4] = "Purple"
+
+# 4 
+del color_list[5]
+
+# 1
+print(color_list)
+
+# 5
+print(color_list[2:8])
+
+# 6
+print("last index is", color_list[-1])
