d7a999c9ec48e3cb42f847eaea945bee4f2b19f8
Sandara <Tababan25@gmail.com>
2024-09-18 03:55:59 -0700

    16_Updated_AnimalsList

---
+++ 16_AnimalSpecies.py
@@ -0,0 +1,21 @@
+animal_species = ["Pig", "Goat", "Sheep", "Cattle", "Buffalo", "Duck", "Turkey"]
+
+
+# 2
+print(animal_species)
+
+# 3
+print(animal_species[2])
+
+# 4
+animal_species[5] = "Gorilla"
+
+# 5
+
+del animal_species[6]
+
+# 2 
+print(animal_species)
+
+# 6
+print("The last index is ", animal_species[-1])
