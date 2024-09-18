425dcc408bde866e9ff5f6bd7f6ba595f63dc543
Sandara <Tababan25@gmail.com>
2024-09-18 03:57:24 -0700

    18_Updated_InventionList

---
+++ 18_InventionList.py
@@ -0,0 +1,20 @@
+invention_list = ["Robotics", "Video", "Electricity", "Penicillin", "Trough Battery", "Printing Press", "Telephone", "Computer"]
+
+
+# 2
+print(invention_list)
+
+# 3
+print(invention_list[6])
+
+# 4
+invention_list[2] = "Lightbulb"
+
+# 5
+del invention_list[5]
+
+# 2
+print(invention_list)
+
+# 6
+print("The last index is ", invention_list[-1])
