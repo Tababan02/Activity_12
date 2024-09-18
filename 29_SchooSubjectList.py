c3b3d7aa21c254dcb5c519d33a26177e8bd18d41
Sandara <Tababan25@gmail.com>
2024-09-18 04:10:54 -0700

    29_Updated_SchoolSubsList

---
+++ 29_SchooSubjectList.py
@@ -0,0 +1,22 @@
+school_subject = ["English",  "Science", "History", "Geography", "Social Studies", "Philosophy", "Health Education", "Health Education", "Economics", "Engineering", "Psychology", "Music"]
+
+# 2
+print(school_subject)
+
+# 3
+print(school_subject[6])
+
+# 4
+school_subject[8] = "Mathematics" 
+
+# 5
+del school_subject[4]
+
+# 2
+print(school_subject)
+
+# 6
+print(school_subject[2:5])
+
+# 7
+print("The last index is ", school_subject[-1])
