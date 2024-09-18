b071c00a51e94f83f01b6dae493be66ebf988d9e
Sandara <Tababan25@gmail.com>
2024-09-18 04:16:17 -0700

    36_Updated_CurrencyList

---
+++ 36_CurrencyList.py
@@ -0,0 +1,21 @@
+currency_list = ["USD", "PHP", "NZD", "GBP", "JPY", "AUD", "CAD", "CNY", "HKD", "TRY"]
+
+
+
+# 2
+print(currency_list)
+
+# 3
+print(currency_list[4])
+
+# 4
+currency_list[7] = "Euro"
+
+# 5
+del currency_list[9]
+
+# 2 
+print(currency_list)
+
+# 6
+print("The last index is ", currency_list[-1])
