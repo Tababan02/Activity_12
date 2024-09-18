930d9169ab866519f2c53451768a2f517f4669ea
Sandara <Tababan25@gmail.com>
2024-09-18 04:15:09 -0700

    33_OceansList

---
+++ 34_OceansList.py
@@ -0,0 +1,20 @@
+oceans_list = ["Atlantic Ocean", "Indian Ocean", "Southern Ocean", "Caribbean Sea", "Sout China Sea"] 
+
+
+# 2
+print(oceans_list)
+
+# 3
+print(oceans_list[3])
+
+# 4
+oceans_list[2] = "Pacific Ocean"
+
+# 5
+del oceans_list[4]
+
+# 2 
+print(oceans_list)
+
+# 6
+print("The last index is ", oceans_list[-1])		
