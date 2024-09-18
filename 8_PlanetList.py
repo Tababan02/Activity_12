0fe617d73e1053553e9e769ca13027dfab3d0d00
Sandara <Tababan25@gmail.com>
2024-09-18 03:37:54 -0700

    8_Updated_PlanetList

---
+++ 8_PlanetList.py
@@ -0,0 +1,19 @@
+planet_list = ["Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune"]
+
+# 2
+print(planet_list)
+
+# 3
+print(planet_list[3])
+
+# 4
+planet_list[7] = "Pluto"
+
+# 5
+del planet_list[4]
+
+# 2 
+print(planet_list)
+
+# 6
+print("The last index is ", planet_list[-1])
