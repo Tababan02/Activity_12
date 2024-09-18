5713bfcaa9a59e12340901479c0f868fd31c4950
Sandara <Tababan25@gmail.com>
2024-09-18 04:13:26 -0700

    32_Updated_HabitatList

---
+++ 32_AnimalHabitatsList.py
@@ -0,0 +1,19 @@
+animal_habitats_list = ["Forest", "Desert", "Polar", "Mountain", "Temperate Forest", "Freshwater", "Ocean", "Rainforest"] 
+
+# 2
+print(animal_habitats_list)
+
+# 3
+print(animal_habitats_list[5])
+
+# 4 
+animal_habitats_list[3] = "Savannah"
+
+# 5
+del animal_habitats_list[6]
+
+# 2
+print(animal_habitats_list)
+
+# 6
+print("The last index is ", animal_habitats_list[-1])
