2c52035593c9f222171934e0ba0cb29e10daa5c8
Sandara <Tababan25@gmail.com>
2024-09-18 04:23:55 -0700

    50_Updated_SpaceMissionList

---
+++ 50_SpaceMissionList.py
@@ -0,0 +1,21 @@
+space_missions_list = ["Sputnik", "Vostok", "Apollo 13", "Mir", "Voyager 1", "Voyager 2", "New Horizons", "Curiosity Rover", "ISS", "Preserverance Rover" ]
+
+
+
+# 2
+print(space_missions_list)
+
+# 3
+print(space_missions_list[7])
+
+# 4 
+space_missions_list[4] = "Apollo 11"
+
+# 5
+del space_missions_list[6]
+
+# 2
+print(space_missions_list)
+
+# 6
+print("The last index is ", space_missions_list[-1])
