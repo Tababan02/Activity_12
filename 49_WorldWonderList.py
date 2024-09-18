e2b7751344bb1637305475fb9e813a34b2723f43
Sandara <Tababan25@gmail.com>
2024-09-18 04:23:26 -0700

    49_Updated_WorldWonderList

---
+++ 49_WorldWonderList.py
@@ -0,0 +1,19 @@
+world_wonders = ["Petra", "Christ of the Redeemer", "Machu Picchu", "Chichen Itza", "Roman Colosseum", "Taj Mahal", "Grand canyon"]
+
+# 2
+print(world_wonders)
+
+# 3
+print(world_wonders[4])
+
+# 4
+world_wonders[2] = "Great Wall of China"
+
+# 5
+del world_wonders[5]
+
+# 2
+print(world_wonders)
+
+# 6
+print("The last index is ", world_wonders[-1])
