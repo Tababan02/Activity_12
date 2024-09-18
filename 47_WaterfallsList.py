97b7d1c370a320f6b36cf969cd0af08d2eb5a4d3
Sandara <Tababan25@gmail.com>
2024-09-18 04:21:47 -0700

    47_Updated_WaterfallsList

---
+++ 47_WaterfallsList.py
@@ -0,0 +1,22 @@
+waterfalls_list = ["Victoria Falls", "Iguazu Falls", "Angel Falls", "Yosemite Falls", "Plitvice Falls", "Seljalandsfoss", "Kakadu Falls", "Gullfoss", "Sutherland falls", "Tugela Falls"]
+
+
+# 2
+print(waterfalls_list)
+
+# 3
+print(waterfalls_list[8])
+
+# 4
+waterfalls_list[5] = "Niagara Falls" 
+
+# 5
+del waterfalls_list[9]
+
+# 2 
+print(waterfalls_list)
+
+# 6
+print("The last index is ", waterfalls_list[-1])
+
+	
