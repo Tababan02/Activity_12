6c8801bf73e43f5ca0d2521ccc506a9c071c2610
Sandara <Tababan25@gmail.com>
2024-09-18 03:46:50 -0700

    12_Updated_LanguageList

---
+++ 12_LanguageSpoken.py
@@ -0,0 +1,23 @@
+language_spoken_list = ["English", "Tagalog", "Chinese", "Hindi", "Bengali", "Portuguese", "Russian", "Japanese", "Yue Chinese", "Vietnamese", "Turkish", "Wu Chinese", "Marathi", "Korean", "Italian", "Southern Min", "French", "Javanese" "Hausa", "Tamil"] 
+
+
+# 2 
+print(language_spoken_list)
+
+# 3 
+print(language_spoken_list[13])
+
+# 4
+language_spoken_list[10] = "Spanish"
+
+# 5
+del language_spoken_list[16]
+
+# 2 
+print(language_spoken_list)
+
+# 6
+print(language_spoken_list[6:11])
+
+# 7
+print("The last index is ", language_spoken_list[-1])
