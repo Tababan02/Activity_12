51f16f74a50f4936f78fea0e2cab358ec8a239e6
Sandara <Tababan25@gmail.com>
2024-09-18 04:18:11 -0700

    40_Updated_FestivalList

---
+++ 40_FestivalCelebrationList.py
@@ -0,0 +1,19 @@
+festival_celebration_list = ["Sinulog", "Pahiyas", "Panagbenga", "Kadayawan", "Ati-atihan", "Oktoberfest", "Diwali", "Mardi Gras", "Chinese New year", "Day of the dead"]
+
+# 2
+print(festival_celebration_list)
+
+# 3
+print(festival_celebration_list[5])
+
+# 4
+festival_celebration_list[3] = "Thanksgiving"
+
+# 5
+del festival_celebration_list[7]
+
+# 2 
+print(festival_celebration_list)
+
+# 6
+print("The last index is " , festival_celebration_list[-1])
