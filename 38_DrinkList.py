8e866dd0564803faf1d413d1f3438214a576b0b3
Sandara <Tababan25@gmail.com>
2024-09-18 04:17:12 -0700

    38_Updated_DrinkList

---
+++ 38_DrinkList.py
@@ -0,0 +1,20 @@
+drink_list = ["Water", "Alcohol", "Milk", "Orange Juice", "Lemonade", "Hot chocolate", "Tea", "Softdrinks"]
+
+
+# 2
+print(drink_list)
+
+# 3
+print(drink_list[4])
+
+# 4
+drink_list[3] = "Coffee"
+
+# 5
+del drink_list[7]
+
+# 2
+print(drink_list)
+
+# 6
+print("The latest index is ", drink_list[-1])
