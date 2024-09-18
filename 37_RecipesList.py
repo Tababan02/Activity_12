9f233f201c9d84c9d7c5bc1666310377ec767042
Sandara <Tababan25@gmail.com>
2024-09-18 04:16:44 -0700

    37_Updated_RecipeList

---
+++ 37_RecipesList.py
@@ -0,0 +1,23 @@
+recipe_list = ["Pancakes", "Omelette", "Avocado toast", "Smoothie", "French Toast", "Caesar Salad", "Cheese Sandwich", "Stir-fry vegetable", "Spaghetti", "Chicken Curry", "Tacos", "Salmon", "Chocolate cookies", "Apple pie", "Tiramisu"]
+
+
+# 2 
+print(recipe_list)
+
+# 3
+print(recipe_list[12])
+
+# 4
+recipe_list[9] = "Lasagna"
+
+# 5
+del recipe_list[11]
+
+# 2 
+print(recipe_list)
+
+# 6
+print(recipe_list[5:10])
+
+# 7
+print("The last index is ", recipe_list[-1])
