c17ce0f63f38671cb1602c8de18599822b8caa2d
Sandara <Tababan25@gmail.com>
2024-09-18 03:57:54 -0700

    19_Updated_HolidayList

---
+++ 19_HolidayList.py
@@ -0,0 +1,19 @@
+holiday_list = ["Independence Day", "Valentines", "New Year", "Labor day", "Easter Day", "All saints Day", "Black Saturday", "Ninoy Aquino Day", "Good Friday", "Thanksgiving Day", "Rizal Day", "Father's Day"]
+
+# 2
+print(holiday_list)
+
+# 3
+print(holiday_list[9])
+
+# 4
+holiday_list[3] = "Christmas"
+
+# 5
+del holiday_list[11]
+
+# 2 
+print(holiday_list)
+
+# 6
+print("The last index is ", holiday_list[-1])
