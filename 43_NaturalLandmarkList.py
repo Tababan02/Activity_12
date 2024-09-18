ec36b5da975f79a870288f28938845abf7feb0d1
Sandara <Tababan25@gmail.com>
2024-09-18 04:19:56 -0700

    43_Updated_LandmarkList

---
+++ 43_NaturalLandmarkList.py
@@ -0,0 +1,20 @@
+natural_landmark_list = ["Mount Everest", "Great barrier reef", "Victoria Falls", "Mount Killmanjaro", "Yellowstone National park", "Salar de Uyuni", "Amazon Rainforest", "Mount Fuji", "Antelope Canyon","Yosemite Valley"] 
+
+# 2
+print(natural_landmark_list)
+
+# 3
+print(natural_landmark_list[8])
+
+# 4
+natural_landmark_list[5] = "Grand Canyon"
+
+# 5
+del natural_landmark_list[9]
+
+# 2
+print(natural_landmark_list)
+
+# 6
+print("The last index is ", natural_landmark_list[-1])
+
