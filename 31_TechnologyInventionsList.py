a5faf1c909774564df8b117b46b4df872ebee761
Sandara <Tababan25@gmail.com>
2024-09-18 04:12:43 -0700

    31_Updated_TechInvention

---
+++ 31_TechnologyInventionsList.py
@@ -0,0 +1,23 @@
+technology_inventions_list = ["AI", "IoT", "AR", "VR", "Drones", "3D Printing", "Blockchain", "5G Technology", "Self Driving Cars", "Voice Assitants", "Biotechnology", "Smart home devices", "AR Glasses", "Gene Editing", "AR Glasses"]
+
+# 2 
+print(technology_inventions_list)
+
+# 3
+print(technology_inventions_list[11]) 
+
+# 4
+technology_inventions_list[8] = "Smartphone"
+
+# 5
+del technology_inventions_list[14]
+
+# 2
+print(technology_inventions_list)
+
+
+# 6
+print(technology_inventions_list[5:10])
+
+# 7
+print("The last index is ", technology_inventions_list[-1])
