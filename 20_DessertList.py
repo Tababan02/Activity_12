f1b0418b7243f1cac70c0c126efe1531ca7e8191
Sandara <Tababan25@gmail.com>
2024-09-18 03:58:32 -0700

    20_Update_DessertList

---
+++ 20_DessertList.py
@@ -0,0 +1,20 @@
+dessert_list = ["Tiramisu cake", "Chocolate Frappe", "Ice Cream", "Apple Cinnamon", "Honey Cheesecake", "Chocolates", "Almond Cookies", "Pies", "Banana Pudding", "S'Mores"]
+
+# 2
+print(dessert_list)
+
+# 3
+print(dessert_list[4])
+
+# 4
+dessert_list[6] = "Chocolate Cake"
+
+# 5
+del dessert_list[3]
+
+# 2
+print(dessert_list)
+
+
+# 6
+print("The last index is ", dessert_list[-1])
