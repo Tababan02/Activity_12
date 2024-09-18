df659f044a7766641da44a9eda5e06867bb6e381
Sandara <Tababan25@gmail.com>
2024-09-18 04:05:25 -0700

    23_Updated_FurnitureList

---
+++ 23_FurnitureList.py
@@ -0,0 +1,19 @@
+furnitures_list = ["Chair", "Table", "Bed", "Dresser", "Closet", "Bookshelf", "Nightstand", "Stool", "Futon", "Wall table", "Relaxing Chair", "Figurines"]
+
+# 2 
+print(furnitures_list)
+
+# 3
+print(furnitures_list[8])
+
+# 4
+furnitures_list[5] = "Sofa"
+
+# 5
+del furnitures_list[10]
+
+# 2
+print(furnitures_list)
+
+# 6
+print("The last index is ", furnitures_list[-1])
