553ffe066c35d9692a365e069f9fb5790c00a693
Sandara <Tababan25@gmail.com>
2024-09-18 04:20:23 -0700

    44_Updated_AnimalsSpeedList

---
+++ 44_LandAnimalSpeedList.py
@@ -0,0 +1,23 @@
+land_animal_speed = ["Elephant - 40km/hr", "Lion - 80km/hr", "Giraffe - 56km/hr", "Horse - 88km/hr", "Gray Wolf - 64km/hr", "Kangaroo - 48km/hr", "Zebra - 64km/hr", "American Bison - 56km/hr", "Hippopotamus - 48km/hr", "Rhinoceros - 56km/hr", "Pronghorn Antelope - 88km/hr", "Gazelle - 80km/hr"]
+
+
+# 2
+print(land_animal_speed)
+
+# 3
+print(land_animal_speed[7])
+
+# 4
+land_animal_speed[9] = "Cheetah - 120km/hr"
+
+# 5
+del land_animal_speed[10]
+
+# 2
+print(land_animal_speed)
+
+# 6
+print(land_animal_speed[3:7])
+
+# 7
+print("The last index is ", land_animal_speed[-1])
