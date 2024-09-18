54b111c481a10c5ffa756ce2e4a9030d1824ea9d
Sandara <Tababan25@gmail.com>
2024-09-18 04:14:39 -0700

    33_Updated_JobTitlesList

---
+++ 33_JobTitlesList.py
@@ -0,0 +1,19 @@
+job_title_list = ["CEO", "Manager", "Data Scientist", "Web Developer", "Database Administrator", "Cloud Engineer", "Systems Analyst", "Specialist", "IT Support", "UX and UI designer"]
+
+# 2 
+print(job_title_list)
+
+# 3 
+print(job_title_list[6])
+
+# 4
+job_title_list[5] = "Software Engineer" 
+
+# 5
+del job_title_list[9]
+
+# 2 
+print(job_title_list)
+
+# 6
+print("The last index is ", job_title_list[-1])
