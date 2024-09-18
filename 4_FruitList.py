16d358b098f69dfea216647bd38ee85739c86a48
Sandara <Tababan25@gmail.com>
2024-09-18 03:33:36 -0700

    4_Updated_FruitList

---
+++ 4_FruitList.py
@@ -0,0 +1,25 @@
+fruit_list = ["strawberries", "apple", "bananas", "avocado", "cherries", 
+              "blueberries", "oranges", "pineapple", "kiwi", "lemon", "guava", "peaches"]
+
+
+# 2
+print(fruit_list)
+
+# 3
+print(fruit_list[9])
+
+# 4
+fruit_list[2] = "Mango"
+
+# 5
+del fruit_list[10]
+
+# 2
+print(fruit_list)
+
+# 6
+print(fruit_list[4:7])
+
+# 7
+print("last index is ", fruit_list[-1])
+
