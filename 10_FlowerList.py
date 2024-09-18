05cb22835876e464f57ce2a268e0fbddc003f45c
Sandara <Tababan25@gmail.com>
2024-09-18 03:40:42 -0700

    10_Updated_FlowerList

---
+++ 10_FlowerList.py
@@ -0,0 +1,19 @@
+flower_list = ["Dahlia", "Sunflower", "Iris", "Tulips", "Begonia", "Carnation", "Lily", "Orchid", "Acanthus", "Marigold"]
+
+# 2 
+print(flower_list)
+
+# 3
+print(flower_list[5])
+
+# 4
+flower_list[8] = "Rose"
+
+# 5
+del flower_list[2]
+
+# 2 
+print(flower_list)
+
+# 6
+print("The last index in ", flower_list[-1])
