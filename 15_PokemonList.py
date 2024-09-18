2944705c5d9e3acf55b57304a45b4991c8b03b1a
Sandara <Tababan25@gmail.com>
2024-09-18 03:54:38 -0700

    15_Updated_PokemonList

---
+++ 15_PokemonList.py
@@ -0,0 +1,23 @@
+pokemon_list = ["Abra", "Accelgor", "Aggron", "Aipom", "Alakazam", "Alcremie", "Applin", "Anorith", "Arbok", "Arboliva", "Arcanine", "Arceus", "Archaludon", "Beldum", "Beedrill"]
+
+
+# 2
+print(pokemon_list)
+
+# 3 
+print(pokemon_list[9])
+
+# 4 
+pokemon_list[12] = "Pikachu"
+
+# 5
+del pokemon_list[10]
+
+# 2 
+print(pokemon_list)
+
+# 6
+print(pokemon_list[4:7])
+
+# 7
+print("The last index is ",pokemon_list[-1])
