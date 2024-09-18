5941f9496dcb00179268b2ae2ef7c89ca5e03dd1
Sandara <Tababan25@gmail.com>
2024-09-18 03:37:17 -0700

    7_Updated_GameList

---
+++ 7_GameList.py
@@ -0,0 +1,22 @@
+game_list = ["GTA", "GTA V", "Borderlands", "Super Mario", "Half-Life", "Half-life 2", "The last of us", "portal", "COD", "Resident Evil", "Final Fantasy", "Hades", "Batman", "Fallout"]
+
+# 2
+print(game_list)
+
+# 3 
+print(game_list[7])
+
+# 4
+game_list[4] = "Minecraft"
+
+# 5
+del game_list[9]
+
+# 2 
+print(game_list)
+
+# 6
+print(game_list[5:10])
+
+# 7
+print("The last index is ", game_list[-1])
