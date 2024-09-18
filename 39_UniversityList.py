262e8bb79f2a2242d8ab763fd238bd79bb6e2ebd
Sandara <Tababan25@gmail.com>
2024-09-18 04:17:46 -0700

    39_Updated_UniversityList

---
+++ 39_UniversityList.py
@@ -0,0 +1,20 @@
+university_list = ["University of Caloocan", "Caloocan City Polytechnic Institute", "Pamantasan ng Lungsod ng Marikina", "University of the east", "Manila Central University", "Far Eastern University", "University of the Philippines", "Ateneo de Manila University", "San beda University", "La Consolacion College"]
+
+
+# 2
+print(university_list)
+
+# 3
+print(university_list[6])
+
+# 4
+university_list[4] = "Harvard University"
+
+# 5
+del university_list[9]
+
+# 2 
+print(university_list)
+
+# 6
+print("The last index is ", university_list[-1])
