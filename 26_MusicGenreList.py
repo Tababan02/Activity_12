0953cdbe99e1b74035446d6fe89d0978ee280154
Sandara <Tababan25@gmail.com>
2024-09-18 04:08:43 -0700

    26_Updated_MusicList

---
+++ 26_MusicGenreList.py
@@ -0,0 +1,23 @@
+music_genre_list = ["Pop", "Rock", "Hip-Hop", "Classical", "Blues", "Country", "Reggae", "Electronic", "R & B", "Folk", "Metal", "Punk", "Latin", "Alternative", "Dance", "Gospel"]
+
+
+# 2
+print(music_genre_list)
+
+# 3
+print(music_genre_list[10])
+
+# 4
+music_genre_list[5] = "Jazz"
+
+# 5
+del music_genre_list[7]
+
+# 2 
+print(music_genre_list)
+
+# 6
+print(music_genre_list[3:8])
+
+# 7
+print("The last index is ", music_genre_list[-1])	
