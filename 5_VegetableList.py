7582ad5ef614a417ffb0ec50f475c504cc1429a8
Sandara <Tababan25@gmail.com>
2024-09-18 03:34:26 -0700

    5_Updated_VegetableList

---
+++ 5_VegetableList.py
@@ -0,0 +1,20 @@
+vegetable_list = ["Carrots", "Tomato", "Potato", "Cabbage", "Corn", "Broccoli", "EggPlant"]
+
+
+# 2
+print(vegetable_list)
+
+# 3 
+print(vegetable_list[5])
+
+# 4 
+vegetable_list[3] = "Spinach."
+
+# 5 
+del vegetable_list[6]
+
+# 2
+print(vegetable_list)
+
+# 6
+print("the last index is ", vegetable_list[-1])
