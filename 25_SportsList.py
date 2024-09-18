026484b1412cbde8d164f796f296788bc9275804
Sandara <Tababan25@gmail.com>
2024-09-18 04:08:01 -0700

    25_SportsList

---
+++ 25_SportsList.py
@@ -0,0 +1,20 @@
+sports_list = ["Soccer", "Baseball", "Tennis", "Golf", "Cricket", "Football", "Hockey", "Volleyball", "Rugby", "Badminton"]
+
+
+# 2
+print(sports_list)
+
+# 3
+print(sports_list[6])
+
+# 4
+sports_list[4] = "Basketball"
+
+# 5
+del sports_list[9]
+
+# 2 
+print(sports_list)
+
+# 6
+print("The last index is ", sports_list[-1])
