45cc54f5313b6d8edfc2d0e48344755204356f69
Sandara <Tababan25@gmail.com>
2024-09-18 04:18:37 -0700

    41_Updated_ContinentList

---
+++ 41_ContinentCountryList.py
@@ -0,0 +1,23 @@
+continent_country_list = ["United States - North America", "Brazil - South America", "China - Asia", "Australia - Australia", "France - Europe", "South Africa - Africa", "India - Asia", "Japan - Asia", "Canada - North America", "Argentina - South America", "Germany - Europe", "Nigeria - Africa", "Mexico - North America", "Egypt - Africa", "New Zealand - Australia"]
+
+# 2
+print(continent_country_list)
+
+# 3
+print(continent_country_list[9])
+
+# 4
+continent_country_list[10] = "Australia"
+
+# 5
+del continent_country_list[12]
+
+# 2 
+print(continent_country_list)
+
+# 6
+print(continent_country_list[4:8])
+
+# 7
+print("The last index is ", continent_country_list[-1])
+#
