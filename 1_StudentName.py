77366e8f5c290a6ac8126539fd6926c25bbfb8e3
Sandara <Tababan25@gmail.com>
2024-09-18 03:19:07 -0700

    1_Updated_StudentName

---
+++ 1_StudentName.py
@@ -0,0 +1,24 @@
+#Python Student name
+student_name_list = ["Sandara", "Dara", "Ara", "Park", "Tababan", "Princess", "Charles", "Luzon", "Rodiel", "Incess", "Doe", "April", "Carl", "Aliyah", "Sofia", "Samantha", "Kevin", "Jay", "Paul", "Gab"]
+
+
+# A
+print(student_name_list)
+
+# B
+print("15th is", student_name_list[14])
+
+# C
+student_name_list[11] = "John"
+
+# D
+del student_name_list[9]
+
+# A
+print(student_name_list)
+
+# E
+print(student_name_list[2:5])
+
+# F
+print("Last index is", student_name_list[-1])
