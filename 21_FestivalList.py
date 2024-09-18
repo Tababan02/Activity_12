f49ec111a2abb85a04cc412805cf5e35ba45a6b6
Sandara <Tababan25@gmail.com>
2024-09-18 04:00:12 -0700

    21_Updated_FestivalList

---
+++ 21_FestivalList.py
@@ -0,0 +1,22 @@
+festival_list = ["Hermosa Festival", "Kuyamis Festival", "Fiesta Tsinoy", "Diyandi Festival", "Feast of the black nazarene", "Pasalamat Festival", "Sinulog Festival", "Ati - Atihan Festival", "Dinagyang Festival", "Cagsawa Festival", "Nangkaraan Festival", "Patunob Festival", "Alimango Festival", "Holy Week", "Moriones Festival"]
+
+# 2
+print(festival_list)
+
+# 3
+print(festival_list[7])
+
+# 4
+festival_list[11] = "Diwali"
+
+# 5
+del festival_list[9]
+
+# 2 
+print(festival_list)
+
+# 6
+print(festival_list[5:12])
+
+# 7
+print("The last index is ", festival_list[-1])
