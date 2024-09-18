9f90a7d3d77f3dcb68721fd488d14ffaf0bcca0f
Sandara <Tababan25@gmail.com>
2024-09-18 03:39:19 -0700

    9_Updated_LanguageList

---
+++ 9_LanguageList.py
@@ -0,0 +1,22 @@
+language_list = ["C#", "C++", "Java", "JavaScript", "SQL", "TypeScript", "Pascal", "R", "Perl", "Swift", "Ruby", "Dart", "Kotlin", "NoSQL"]
+
+# 2
+print(language_list)
+
+# 3
+print(language_list[11])
+
+# 4 
+language_list[9] = "Python"
+
+# 5 
+del language_list[13]
+
+# 2 
+print(language_list)
+
+# 6
+print(language_list[5:12])
+
+# 7
+print("The last index is ", language_list[-1])
