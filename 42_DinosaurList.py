eb9e4875b1e88bf653dd21b2790cb510d12fb4d3
Sandara <Tababan25@gmail.com>
2024-09-18 04:19:20 -0700

    42_Updated_DinosaurList

---
+++ 42_DinosaurList.py
@@ -0,0 +1,20 @@
+dinosaur_list = ["Triceratops", "Velociraptor", "Brachiosaurus", "Stegosaurus", "Diplodocus", "Ankylosaurus", "Allosaurus", "Spinosaurus"]
+
+
+# 2 
+print(dinosaur_list)
+
+# 3
+print(dinosaur_list[4])
+
+# 4
+dinosaur_list[6] = "Tyrannousaurus Rex"
+
+# 5
+del dinosaur_list[7]
+
+# 2
+print(dinosaur_list)
+
+# 6
+print("The last index is ",dinosaur_list[-1])
