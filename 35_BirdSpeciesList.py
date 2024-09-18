d455b78832e2d6e0ee94f1c8bb49f9aae5094027
Sandara <Tababan25@gmail.com>
2024-09-18 04:15:40 -0700

    35_Updated_BirdSpeciesList

---
+++ 35_BirdSpeciesList.py
@@ -0,0 +1,23 @@
+bird_species_list = ["Philippine Tarsier", "Hawk-Eagle", "Cockatoo", "Eagle-Owl", "Tawny breated", "Black-naped Oriole", "Yellow Breasted Dove", "Gallicolumba Crinigera", "Green-Winged", "Whiskered Tern", "Barn Swallow", "Alcedo Atthis"]
+
+
+# 2
+print(bird_species_list)
+
+# 3
+print(bird_species_list[9])
+
+# 4
+bird_species_list[6] = "Eagle"
+
+# 5
+del bird_species_list[7]
+
+# 2
+print(bird_species_list)
+
+# 6
+print(bird_species_list[3:7])
+
+# 7
+print("The last index is ", bird_species_list[-1])
