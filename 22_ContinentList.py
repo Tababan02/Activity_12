ab0814a3396a3c60ac0c4e2086a297a80dac5961
Sandara <Tababan25@gmail.com>
2024-09-18 04:03:53 -0700

    22_Updated_ContinentList

---
+++ 22_ContinentList.py
@@ -0,0 +1,25 @@
+continent_list = ["Africa", "North America",
+                  "South America", "Europe",
+                  "Antartica", "Australia"]
+
+
+# 2 
+print(continent_list)
+
+# 3 
+print(continent_list[4])
+
+# 4
+continent_list[2] = "Africa"
+
+# 5
+del continent_list[4]
+
+# 2 
+print(continent_list)
+
+# 6
+print("The Last index is ", continent_list[-1])
+
+
+
